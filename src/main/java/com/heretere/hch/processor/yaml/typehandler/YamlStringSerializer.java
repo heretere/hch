f2dfea51efb0ce07dfd46d60c5a94581c90ef2bc
Imagine <70789387+imaginedevMC@users.noreply.github.com>
2021-01-08 14:24:38 -0500

    Add author javadocs (#1)

--- src/main/java/com/heretere/hch/processor/yaml/typehandler/YamlStringSerializer.java
+++ src/main/java/com/heretere/hch/processor/yaml/typehandler/YamlStringSerializer.java
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
+ * @author imaginedevMC
  */
 
 package com.heretere.hch.processor.yaml.typehandler;
